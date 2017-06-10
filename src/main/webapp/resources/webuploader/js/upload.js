06b215f2a375074c60ae994a248dd446c33c6c1f
AnjSii <spiritanjsii@gmail.com>
2017-06-10 15:39:02 +0800

    完成上传的实现。

--- src/main/webapp/resources/webuploader/js/upload.js
+++ src/main/webapp/resources/webuploader/js/upload.js
@@ -53,25 +53,6 @@
                 return support;
             } )(),
 
-            // 检测是否已经安装flash，检测flash的版本
-            flashVersion = ( function() {
-                var version;
-
-                try {
-                    version = navigator.plugins[ 'Shockwave Flash' ];
-                    version = version.description;
-                } catch ( ex ) {
-                    try {
-                        version = new ActiveXObject('ShockwaveFlash.ShockwaveFlash')
-                                .GetVariable('$version');
-                    } catch ( ex2 ) {
-                        version = '0.0';
-                    }
-                }
-                version = version.match( /\d+/g );
-                return parseFloat( version[ 0 ] + '.' + version[ 1 ], 10 );
-            } )(),
-
             supportTransition = (function(){
                 var s = document.createElement('p').style,
                     r = 'transition' in s ||
@@ -86,60 +67,9 @@
             // WebUploader实例
             uploader;
 
-        if ( !WebUploader.Uploader.support('flash') && WebUploader.browser.ie ) {
-
-            // flash 安装了但是版本过低。
-            if (flashVersion) {
-                (function(container) {
-                    window['expressinstallcallback'] = function( state ) {
-                        switch(state) {
-                            case 'Download.Cancelled':
-                                alert('您取消了更新！')
-                                break;
-
-                            case 'Download.Failed':
-                                alert('安装失败')
-                                break;
-
-                            default:
-                                alert('安装已成功，请刷新！');
-                                break;
-                        }
-                        delete window['expressinstallcallback'];
-                    };
-
-                    var swf = '/flash/expressInstall.swf';
-                    // insert flash object
-                    var html = '<object type="application/' +
-                            'x-shockwave-flash" data="' +  swf + '" ';
-
-                    if (WebUploader.browser.ie) {
-                        html += 'classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" ';
-                    }
-
-                    html += 'width="100%" height="100%" style="outline:0">'  +
-                        '<param name="movie" value="' + swf + '" />' +
-                        '<param name="wmode" value="transparent" />' +
-                        '<param name="allowscriptaccess" value="always" />' +
-                    '</object>';
-
-                    container.html(html);
-
-                })($wrap);
-
-            // 压根就没有安转。
-            } else {
-                $wrap.html('<a href="http://www.adobe.com/go/getflashplayer" target="_blank" border="0"><img alt="get flash player" src="http://www.adobe.com/macromedia/style_guide/images/160x41_Get_Flash_Player.jpg" /></a>');
-            }
-
-            return;
-        } else if (!WebUploader.Uploader.support()) {
-            alert( 'Web Uploader 不支持您的浏览器！');
-            return;
-        }
-
         // 实例化
         uploader = WebUploader.create({
+            fileVal: 'MyFiledName',
             pick: {
                 id: '#filePicker',
                 label: '点击选择图片'
@@ -149,18 +79,15 @@
             },
             dnd: '#dndArea',
             paste: '#uploader',
-            swf: BASE_URL + '/resources/webuploader/flash/Uploader.swf',
             chunked: false,
             chunkSize: 512 * 1024,
             server: BASE_URL + '/upload.htm',
             // runtimeOrder: 'flash',
-
-            // accept: {
-            //     title: 'Images',
-            //     extensions: 'gif,jpg,jpeg,bmp,png',
-            //     mimeTypes: 'image/*'
-            // },
-
+             accept: {
+                 title: 'Images',
+                 extensions: 'gif,jpg,jpeg,bmp,png',
+                 mimeTypes: 'image/*'
+             },
             // 禁掉全局的拖拽功能。这样不会出现图片拖进页面的时候，把图片打开。
             disableGlobalDnd: true,
             fileNumLimit: 300,
